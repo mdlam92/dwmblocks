9e2f57b45531d7d00d1f981f7e7bb3d8dcc2ca10
mlamunion <mlamunion@rivian.com>
2022-04-19 13:48:11 -0700

    yeet

--- blocks.h
+++ blocks.h
@@ -1,11 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/						     /*Update Interval*/		/*Update Signal*/
-	{"♪ ",       "spotify_status.sh",						5,					0},
-	{"VOL: ",    "volume_check.sh",							5,					44},
 
 	{"CPU: ",    "cpu_temp.sh",								5,					0},
-	{"MEM: ",    "mem_status.sh",							5,					0},
+	{"MEM: ",    "free -mh | grep Mem | awk '{print $3, \"/\", $2}'",	5,		0},
 	{"GPU: ",   "gpu_temp.sh",								5,					0},
 
 	{"",        "date '+%Y-%m-%d %H:%M:%S'",				1,					0},
